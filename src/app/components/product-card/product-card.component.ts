ee4265f848500c67529f1db596fd5094065badcb
Bilal <bilal.email>
2024-10-21 14:24:15 +0500

    changes

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { DecimalPipe, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GlobalStateService } from '../../shared/services/state/global-state.service';
@@ -9,11 +9,12 @@ import { ToastrService } from 'ngx-toastr';
   selector: 'app-product-card',
   standalone: true,
   imports: [NgIf, RouterLink],
+  providers: [DecimalPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
-  constructor(private globalStateService: GlobalStateService, private mainServices: MainServicesService, private toastr: ToastrService) { }
+  constructor(private decimalPipe: DecimalPipe, private globalStateService: GlobalStateService, private mainServices: MainServicesService, private toastr: ToastrService) { }
   @Input() postData: any = {}
   @Input({ required: true }) postDetialUrl: string = ""
   wishList: any = []
@@ -21,7 +22,10 @@ export class ProductCardComponent {
   getYear(date: string) {
     return new Date(date).getFullYear();
   }
+  formatPrice(price: any) {
+    return this.decimalPipe.transform(price, '1.0-0') || '0';
 
+  }
   toggleWishlist(item: any) {
     this.globalStateService.wishlistToggle(item.id);
     this.globalStateService.currentState.subscribe(state => {
